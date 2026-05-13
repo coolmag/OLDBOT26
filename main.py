b92e8972a706dd48e3ae46d1113e36d64ee222ff
coolmag <54004066+coolmag@users.noreply.github.com>
2026-05-13 22:22:41 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -32,14 +32,15 @@ async def lazy_startup_tasks(app: FastAPI):
     tg_app = app.state.tg_app
     
     commands = [
-        BotCommand("radio", "🎲 Случайная волна"), 
-        BotCommand("play", "🔎 Найти трек"), 
-        BotCommand("skip", "⏭ Следующий трек"), 
-        BotCommand("stop", "🛑 Остановить"), 
+        BotCommand("radio", "🎲 Случайная волна"),
+        BotCommand("play", "🔎 Найти трек"),
+        BotCommand("artist", "🎤 Режим одного исполнителя"),
+        BotCommand("skip", "⏭ Следующий трек"),
+        BotCommand("stop", "🛑 Остановить"),
+        BotCommand("set_genre", "💿 Сменить жанр (Админ)"),
         BotCommand("admin", "⚙️ Настройки"),
         BotCommand("quiz", "🎮 Игра 'Угадай мелодию'")
-    ]
-    
+    ]    
     connected = False
     attempt = 1
     while not connected:
